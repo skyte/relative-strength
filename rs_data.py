7090a3639da125ea9a25954d7bd5b3ab53178993
skyte <trueskyte@gmail.com>
2022-01-08 14:22:54 +0100

    Try to make the yahoo data split adjusted.

--- rs_data.py
+++ rs_data.py
@@ -254,7 +254,7 @@ def get_yf_data(security, start_date, end_date):
         ticker_data = {}
         ticker = security["ticker"]
         escaped_ticker = escape_ticker(ticker)
-        df = yf.download(escaped_ticker, start=start_date, end=end_date)
+        df = yf.download(escaped_ticker, start=start_date, end=end_date, auto_adjust=True)
         yahoo_response = df.to_dict()
         timestamps = list(yahoo_response["Open"].keys())
         timestamps = list(map(lambda timestamp: int(timestamp.timestamp()), timestamps))
